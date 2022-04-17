64e996e7d442f1a74e24c59601a440efe02abd61
diskit <diskit.angmo@catalyst.sh>
2022-04-17 22:53:00 +0530

    updated style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from "react";
 import "./App.css";
 import Banner from "./Components/BannerSection/Banner";
 import Navbar from "./Components/Navbar/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    h2: {
+      ["@media (max-width:600px)"]: {
+        fontSize: "35px !important",
+      },
+    },
+    h5: {
+      ["@media (max-width:600px)"]: {
+        fontSize: "18px !important",
+      },
+    },
+  },
+});
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Banner />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Banner />
+      </ThemeProvider>
     </div>
   );
 };
